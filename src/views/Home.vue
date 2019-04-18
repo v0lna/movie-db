1f6abbdca1fb0cf175e41559e5d663737c135ef7
Sergey <graylebedinetss@gmail.com>
2019-04-18 18:27:12 +0300

    configuration project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>{{movie.title}}</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import config from "@/config";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      movie: {}
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData: async function() {
+      try {
+        const res = await fetch(
+          `https://api.themoviedb.org/3/movie/550?api_key=${config.api_key}`
+        );
+        this.movie = await res.json();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+  components: {}
 };
 </script>
