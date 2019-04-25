dcd9e28e71caaf02ada42ca8ab990dc21d8a5d64
Sergey <graylebedinetss@gmail.com>
2019-04-25 19:55:58 +0300

    after reinstall i18n

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,10 @@
 module.exports = {
   pluginOptions: {
     i18n: {
-      locale: "uk",
-      fallbackLocale: "ru",
-      localeDir: "locales",
-      enableInSFC: false
+      locale: 'ru',
+      fallbackLocale: 'uk',
+      localeDir: 'locales',
+      enableInSFC: true
     }
-  },
-  chainWebpack: config => {
-    config.module
-      .rule("i18n")
-      .resourceQuery(/blockType=i18n/)
-      .type("javascript/auto")
-      .use("i18n")
-      .loader("@kazupon/vue-i18n-loader")
-      .end();
   }
-};
+}
